ccbe308be6ba090925bee6d78abe78e770934953
KeeganVerwey <keeganverwey@gmail.com>
2022-02-09 01:01:12 +0200

    Collapse Panel
    Collapse Panel UI

--- UI/ClientApp/src/app/pages/home/home.component.ts
+++ UI/ClientApp/src/app/pages/home/home.component.ts
@@ -1,6 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, OnInit } from '@angular/core';
 
+export interface NgbPanelChangeEvent {
+  nextState: boolean;
+  panelId: string;
+  preventDefault: () => void;
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
@@ -8,8 +14,10 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 
 export class HomeComponent implements OnInit {
-  constructor(http: HttpClient){
+  lastPanelId: string = null;
+  defaultPanelId: string = "panel2";
 
+  constructor(http: HttpClient){
     http.get<any>('http://peairbrakes.vfsolutions.co.za/api/weatherforecast').subscribe(result => {
       console.log(result);
     }, error => console.error(error));
@@ -19,4 +27,24 @@ export class HomeComponent implements OnInit {
 
   }
 
+  panelShadow($event: NgbPanelChangeEvent, shadow) {
+    const { nextState } = $event;
+    const activePanelId = $event.panelId;
+    const activePanelElem = document.getElementById(activePanelId);
+
+    if (!shadow.isExpanded(activePanelId)) {
+      activePanelElem.parentElement.classList.add("open");
+    }
+
+    if (!this.lastPanelId) this.lastPanelId = this.defaultPanelId;
+    if (this.lastPanelId) {
+      const lastPanelElem = document.getElementById(this.lastPanelId);
+      if (this.lastPanelId === activePanelId && nextState === false)
+        activePanelElem.parentElement.classList.remove("open");
+      else if (this.lastPanelId !== activePanelId && nextState === true) {
+        lastPanelElem.parentElement.classList.remove("open");
+      }
+    }
+    this.lastPanelId = $event.panelId;
+  }
 }
